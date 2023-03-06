d12007d6228172626ff1d6fac61665b2d3872762
PC670 <ismoilovashahina@gmail.com>
2023-03-06 12:42:58 +0500

    adapter

--- app/src/main/java/uz/itschool/hunger/adapter/FoodTypesAdapter.kt
+++ app/src/main/java/uz/itschool/hunger/adapter/FoodTypesAdapter.kt
@@ -1,7 +1,9 @@
 package uz.itschool.hunger.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Text
@@ -11,13 +13,22 @@ import uz.itschool.hunger.model.FoodTypes
 class FoodTypesAdapter(var listFoods:List<FoodTypes>): RecyclerView.Adapter<FoodTypesAdapter.MyHolder>() {
     class MyHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         var name = itemView.findViewById<TextView>(R.id.name)
+        var img = itemView.findViewById<ImageView>(R.id.image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.adapter_food, parent, false)
+
+        return MyHolder(view)
+
     }
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
+
+        var cort = listFoods.get(position)
+
+        holder.img.setImageResource(cort.img)
+        holder.name.text = cort.naem
 
     }
 
