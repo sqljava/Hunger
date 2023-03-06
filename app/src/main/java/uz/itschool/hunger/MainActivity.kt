fcc40c3c8db78d3bd568208eccc3db088e8a0e99
Khamroev001 <sattorkhamroev@gmail.com>
2023-03-06 12:48:50 +0500

    a

--- app/src/main/java/uz/itschool/hunger/MainActivity.kt
+++ app/src/main/java/uz/itschool/hunger/MainActivity.kt
@@ -2,10 +2,21 @@ package uz.itschool.hunger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.itschool.hunger.databinding.ActivityMainBinding
+import uz.itschool.hunger.model.Reklama
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    var reklamalar = mutableListOf<Reklama>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    fun reklamaMassiv(){
+
     }
 }
